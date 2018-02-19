515a8e07780f50a96f91b7ffb9d0418e5c912162
Alexander Pavlov <a.pavlov@mretailer.ru>
2018-02-19 20:17:47 +0300

    init

---
+++ VkApiTest/Api/VkApi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RestSharp;
+
+namespace VkApiTest.Api
+{
+    public class VkApi
+    {
+        public RestClient Client;
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+
+        public VkApi()
+        {
+            Client = new RestClient();
+            Client.BaseUrl = new Uri("https://api.vk.com/method/");
+        }
+
+        public IRestRequest CreateRequest(String Resource)
+        {
+            return new RestRequest { Resource = Resource }
+                .AddParameter("client_id", this.ClientId)
+                .AddParameter("client_secret", this.ClientSecret);
+        }
+
+        public SignupResponse Signup(SignupRequest RequestBody) {
+            var req = this.CreateRequest("auth.signup");
+            req.AddObject(RequestBody);
+            var res = this.Client.Execute<SignupResponse>(req);
+            Console.WriteLine(res.Content);
+            return res.Data;
+        }
+    }
+}
